4fa8c86abbff27c47b40a3f4b7894b3d42901330
LiamMcMurtry <72530818+LiamMcMurtry@users.noreply.github.com>
2020-10-09 10:48:34 -0400

    Update FE595_Web_Scraper.py

--- FE595_Web_Scraper.py
+++ FE595_Web_Scraper.py
@@ -3,17 +3,19 @@
 
 # In[123]:
 
-
 import requests
 import pandas as pd
 def Web_Scraping():
+    url = 'http://3.95.249.159:8000/random_company'
     com = pd.DataFrame(columns=['Name', 'Purpose'])
     for i in range(50):
         # Here we make a request using the URL provided. Every time this URL is executed, it will generate info for a new, random company
-        req = requests.get('http://3.95.249.159:8000/random_company').text
-        new = req.split('<ol>')[1].split('</ol>')[0].split('<li>')
+        req = requests.get(url).text
+        res = req.split('<ol>')[1]
+        ren = res.split('</ol>')[0]
+        red = ren.split('<li>')
         list = {}
-        for n in new:
+        for n in red:
             data = n.replace('\n', '').replace('</li>', '').strip(' ')
             if 'Name:' in data:
                 list['Name'] = data.split('Name: ')[1]
